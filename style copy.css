71de4836ce08c9683c752da11552c6876792015b
Ryan J <ryanjayako@gmail.com>
2010-12-27 02:29:04 -0500

    testing from mac

---
+++ style copy.css
@@ -0,0 +1,28 @@
+@charset "UTF-8";
+
+.footernav {font-family: Verdana, Geneva, sans-serif; font-size: 12px; color: #555152; font-weight: bold; }
+.copyrightfont {font-family: Verdana, Geneva, sans-serif;font-size: 9px;color: #555152;}
+
+.titlefont {font-family: Verdana, Geneva, sans-serif;font-size: 14px;color: #555152;font-weight: bold;}
+.quotefont {font-family: Verdana, Geneva, sans-serif;font-size: 14px;color: #2e2633;font-weight: bold;}
+.authorfont {font-family: Verdana, Geneva, sans-serif;font-size: 14px;color: #2e2633;padding-left:15px;}
+.userquotefont {font-family: Verdana, Geneva, sans-serif;font-size: 14px;color: #99173C;}
+
+
+body {background-color: #DCE9BE;}
+.noresultsfont {color: #cdd8b2;font-family: Verdana, Geneva, sans-serif;font-size: 36px;font-weight:bold;}
+
+#searchwrapper {width:781px; height:61px;background-image:url(searchbar.png);background-repeat:no-repeat; padding:0px;margin:0px;position:relative; }
+ 
+#searchwrapper form { display:inline ; }
+ 
+.searchbox {border:0px; background-color:transparent; position:absolute; top:6px;left:17px;width:635px;height:41px;
+font-family:Verdana, Geneva, sans-serif;font-size:24px;color:#999;}
+ 
+.searchbox_submit {border:0px; background-color:transparent;position:absolute; top:10px;left:635px;width:130px;height:41px;}
+
+.inputpassuser{
+	
+	width : 
+	
+	}
